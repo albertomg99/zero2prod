90c3f384f2c5330ff374b101cb59ade47a48057e
Alberto Martinez <albertomg@gmail.com>
2021-10-19 16:45:49 +0000

    --- Auto-Git Commit ---

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -15,9 +15,25 @@ async fn health_check_works() {
     spawn_app().await().expect("Failed to spawn our app");
 
     // We need to bring in `reqwest`
-// to perform HTTP requests against our application.
-//
-// Use `cargo add reqwest --dev --vers 0.11` to add
-// it under `[dev-dependencies]` in Cargo.toml
-let client = reqwest::Client::new();
+    // to perform HTTP requests against our application.
+    //
+    // Use `cargo add reqwest --dev --vers 0.11` to add
+    // it under `[dev-dependencies]` in Cargo.toml
+    let client = reqwest::Client::new();
+
+    //Act
+    let response = client
+        .get("http://127.0.0.1:8080/health_check")
+        .send()
+        .await()
+        .expect("Failed to execute request")
+    
+    //Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_lenght());
+}
+
+// Launch our application in the background ~somehow~
+async fn spawn_app() -> std::io::Result<()> {    
+    todo!()
 }
