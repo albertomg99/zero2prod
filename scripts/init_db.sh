254e6bca3fc09d3a13cd52f197e7a4ec2c838a60
Alberto Martinez <albertomg@gmail.com>
2021-10-20 10:38:22 +0000

    --- Auto-Git Commit ---

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -14,8 +14,18 @@ docker run \
     -e POSTGRES_USER=${DB_USER} \
     -e POSTGRES_PASSWORD=${DB_PASSWORD} \
     -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}":5432 \
-    -d postgres:alpine \
-    -v /home/alberto/zero2prod/data:/var/lib/postgresql/data \
-    postgres -N 1000
+    -p "${DB_PORT}":5432 -v /home/alberto/zero2prod/data:/var/lib/postgresql/data \
+    -d postgres:alpine postgres -N 1000
 # ^ Increased maximum number of connections for testing purposes
+#-v /home/alberto/zero2prod/data:/var/lib/postgresql/data \
+
+# Keep pinging Postgres until it's ready to accept commands
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+>&2 echo "Postgres is still unavailable - sleeping"
+sleep 1
+done
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+sqlx database create
