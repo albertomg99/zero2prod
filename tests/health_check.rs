fc742e827b8501150f00525fa563b3b1dd24f14f
Alberto Martinez <albertomg@gmail.com>
2021-10-20 07:05:02 +0000

    --- Auto-Git Commit ---

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -12,7 +12,7 @@
 #[actix_rt::test]
 async fn health_check_works() {
     //Arrange
-    spawn_app().await().expect("Failed to spawn our app");
+    spawn_app().await.expect("Failed to spawn our app");
 
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application.
@@ -25,15 +25,15 @@ async fn health_check_works() {
     let response = client
         .get("http://127.0.0.1:8080/health_check")
         .send()
-        .await()
-        .expect("Failed to execute request")
-    
+        .await
+        .expect("Failed to execute request");
+
     //Assert
     assert!(response.status().is_success());
-    assert_eq!(Some(0), response.content_lenght());
+    assert_eq!(Some(0), response.content_length());
 }
 
 // Launch our application in the background ~somehow~
-async fn spawn_app() -> std::io::Result<()> {    
+async fn spawn_app() -> std::io::Result<()> {
     todo!()
 }
