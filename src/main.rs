214657c71dadde7fd038cd600d370584a6b7e085
Alberto Martinez <albertomg@gmail.com>
2021-10-20 16:23:07 +0000

    --- Auto-Git Commit ---

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 //! src/main.rs
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::{configuration::get_configuration, startup::run};
 
@@ -7,11 +7,11 @@ use zero2prod::{configuration::get_configuration, startup::run};
 async fn main() -> std::io::Result<()> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgConnection::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
     // We have removed the hard-coded `8000` - it's now coming from our settings!
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind 8080 port");
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
