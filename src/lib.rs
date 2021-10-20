61eb5c0955e7cf8943b6fda18b091e9567b24451
Alberto Martinez <albertomg@gmail.com>
2021-10-20 10:08:03 +0000

    --- Auto-Git Commit ---

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use std::net::TcpListener;
 #[derive(serde::Deserialize)]
 struct FormData {
     email: String,
-    name: String,
+    name: Option<String>,
 }
 
 async fn health_check() -> impl Responder {
