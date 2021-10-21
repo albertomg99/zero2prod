314af29878e82c0835afe0d8de7d047437cabc89
Alberto Martinez <albertomg@gmail.com>
2021-10-21 14:30:30 +0000

    --- Auto-Git Commit ---

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,6 +9,17 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "RID:{}> Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name
+    );
+    log::info!(
+        "RID:{}> Saving new subscriber details in the database",
+        request_id
+    );
     match sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at)
                     VALUES ($1, $2, $3, $4)"#,
@@ -21,11 +32,11 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> im
     .await
     {
         Ok(_) => {
-            log::info!("Saving new subscriber '{}' details in the DB", form.name);
+            log::info!("RID:{}> New subscriber details saved in the DB", request_id);
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!("Failed to execute query: {:?}", e);
+            log::error!("RID:{}> Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
