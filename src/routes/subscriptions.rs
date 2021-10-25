6caa8a64e84d3809c1243a8d92e42340556b1397
Alberto Martinez <albertomg@gmail.com>
2021-10-25 16:35:19 +0000

    --- Auto-Git Commit ---

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,34 +9,19 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
-    let request_id = Uuid::new_v4();
-    // Spans, like logs, have an associated level
-    // `info_span` creates a span at the info-level
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id,
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id=%Uuid::new_v4(),
         subscriber_email = %form.email,
-        subscriber_name= %form.name
-    );
-    // Using `enter` in an async function is a recipe for disaster!
-    // Bear with me for now, but don't do this at home.
-    // See the following section on `tracing-futures`
-    let _request_span_guard = request_span.enter();
-    // We do not call `.enter` on query_span!
-    // `.instrument` takes care of it at the right moments
-    // in the query future lifetime
+        subscriber_name = %form.name
+    )
+)]
+
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
     let query_span = tracing::info_span!("Saving new subscriber details in the database");
-    tracing::info!(
-        "RID:{}> Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
-    );
-    tracing::info!(
-        "RID:{}> Saving new subscriber details in the database",
-        request_id
-    );
+
     match sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at)
                     VALUES ($1, $2, $3, $4)"#,
@@ -49,12 +34,9 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> im
     .instrument(query_span)
     .await
     {
-        Ok(_) => {
-            tracing::info!("RID:{}> New subscriber details saved in the DB", request_id);
-            HttpResponse::Ok().finish()
-        }
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
-            tracing::error!("RID:{}> Failed to execute query: {:?}", request_id, e);
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
