1755cc4ff696cf2db0f38c87f29c5e59b10d1502
Alberto Martinez <albertomg@gmail.com>
2021-10-28 17:00:55 +0000

    --- Auto-Git Commit ---

---
+++ src/routes/client.rs
@@ -0,0 +1,42 @@
+use actix_web::{web, HttpResponse, Responder};
+use chrono::Utc;
+use sqlx::MySqlPool;
+use uuid::Uuid;
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(subscriber_email = %form.email, subscriber_name = %form.name)
+)]
+
+pub async fn nou_client(form: web::Form<FormData>, pool: web::Data<MySqlPool>) -> impl Responder {
+    match insert_client(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+#[tracing::instrument(name = "Saving new client to DB", skip(form, pool))]
+
+pub async fn insert_client(pool: &MySqlPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"INSERT INTO subscriptions (id, email, name, subscribed_at)
+                    VALUES ($1, $2, $3, $4)"#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
+}
