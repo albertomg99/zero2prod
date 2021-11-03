ff35789ba7f51ad64aee377f96717790b5338e05
Alberto Martinez <albertomg@gmail.com>
2021-11-03 08:41:55 +0000

    --- Auto-Git Commit ---

--- src/routes/client.rs
+++ src/routes/client.rs
@@ -40,3 +40,31 @@ pub async fn insert_client(pool: &MySqlPool, form: &FormData) -> Result<(), sqlx
     })?;
     Ok(())
 }
+
+#[derive(sqlx::FromRow)]
+struct Client {
+    Codiclient: String,
+    Nom: String,
+    Cognom1: String,
+    Cognom2: String,
+    Telefon1: String,
+}
+
+pub async fn llista_client(
+    codi_client: web::Form<String>,
+    pool: web::Data<MySqlPool>,
+) -> impl Responder {
+    match consulta_client(&pool, &codi_client).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+pub async fn consulta_client(pool: &MySqlPool, codi_client: &String) -> Result<(), sqlx::Error> {
+    sqlx::query(
+        r#"SELECT Codiclient, Nom, Cognom1, Cognom2, Telefon1 FROM clients.ClientsDades WHERE Codiclient = ?"#,
+    )
+    .bind(codi_client);
+
+    Ok(())
+}
